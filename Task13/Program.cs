5bb2412a9625defc2757a9ef0b67cce5b0f5548d
Sergei Youruizzu <icykle.wolf@gmail.com>
2023-02-16 21:04:10 +0300

    Added Task13 solution

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+int num = new Random().Next(1, 1000000);
+Console.WriteLine($"Изначальное число: {num}");
+int count = 0;
+int countAssist = num;
+int third = num;
+
+while (countAssist > 0){
+    countAssist /= 10;
+    count++;
+}
+
+Console.WriteLine($"Длина числа: {count} цифр");
+
+if (count >= 3) {
+    while (count > 3){
+        third /= 10;
+        count--;
+    }
+    third %= 10;
+    Console.WriteLine($"{num} -> третья цифра = {third}");
+}
+else { Console.WriteLine($"{num} -> третьей цифры нет");}
